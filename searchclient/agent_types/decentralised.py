54999ffae06096bffe0f865f97353483267c6674
johannino <johanninov@gmail.com>
2023-03-09 15:41:29 +0100

    Update decentralised.py

--- searchclient/agent_types/decentralised.py
+++ searchclient/agent_types/decentralised.py
@@ -24,4 +24,50 @@ def decentralised_agent_type(level, initial_state, action_library, goal_descript
     # use 'print(joint_action_to_string(joint_action), flush=True)' to send a joint_action to the server and
     # use 'parse_response(read_line())' to read back an array of booleans indicating whether each individual action
     #   in the joint action succeeded.
-    raise NotImplementedError()
+
+    planning_success, plan = graph_search(initial_state, action_set, goal_description, frontier)
+
+    #print(plan)
+    if not planning_success:
+        print("Unable to solve level.", file=sys.stderr)
+        return
+
+    print(f"Found solution of length {len(plan)}", file=sys.stderr)
+
+    num_agents = level.num_agents
+
+
+
+
+    # Compute a plan for each agent
+    plans = []
+    for i in range(num_agents):
+        agent_postion, agent_char = initial_state.agent_positions[i]
+        #print("")
+        #print(f"her er agent= {agent_postion}")
+        #print("")
+        #agent_idx = initial_state.agent_at(agent_postion)
+        agent_color = level.colors[agent_char]
+        monochrome_problem = initial_state.color_filter(agent_color)
+        planning_success, pi  = graph_search(monochrome_problem, action_set, goal_description, frontier)
+        #print("")
+        #print(f"her er pi = {pi}")
+        #print("")
+        plans.append(pi)
+
+
+    while all(plans):
+        for joint_action in plan:
+
+            # Send the joint action to the server
+            print(joint_action_to_string(joint_action), flush=True)
+            # Uncomment the below line to print the executed actions to the command line for debugging purposes
+            # print(joint_action_to_string(joint_action), file=sys.stderr, flush=True)
+
+            # Read back whether the agents succeeded in performing the joint action
+            execution_successes = parse_response(read_line())
+            if False in execution_successes:
+                print("Execution failed! Stopping...", file=sys.stderr)
+                # One of the agents failed to execute their action.
+                # This should not occur in classical planning and we therefore just abort immediately
+                return
